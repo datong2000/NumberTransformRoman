9a738dc5abb5f80ea3dd27aa2848d571f951e9b6
大童 <2829099680@qq.com>
2022-02-21 13:36:35 +0800

    初始化项目

--- README.md
+++ README.md
@@ -1,2 +1,59 @@
 # NumberTransformRoman
-一个用于数字转换成罗马数字的API
+
+一个用于数字转换成罗马数字的 `Class` 类，你可以使用 `transformRoman` 来进行数字转换罗马数字，也可以使用 `calculation` 来进行计算再转罗马数字.
+
+---
+
+##### 引入
+
+```Javascript
+import NumberTransformRoman as ntr from 'NumberTransformRoman'  // ntr 为 NumberTransformRoman 简写
+```
+
+##### 使用方法
+
+###### transformRoman
+
+> transformRoman(type: number | string ) 数字转换罗马数字
+
+```Javascript
+    import NumberTransformRoman as ntr from 'NumberTransformRoman'  // ntr 为 NumberTransformRoman 简写
+    
+    // 数字类型
+    new ntr().transformRoman(990108010)  // 结果为:CMXC#,C#VIII,#X#
+
+    // 或者
+
+    // 字符串类型
+    new ntr().transformRoman('80108010')  // 结果为:LXXX#,C#VIII,#X#
+```
+
+###### calculation
+
+> calculation(type: function ) 数字计算表达式
+
+```Javascript
+    import NumberTransformRoman as ntr from 'NumberTransformRoman'  // ntr 为 NumberTransformRoman 简写
+    
+    // 参数为匿名函数表达式
+    new ntr().calculation(() => 2 * 6 / 3)  // 结果为:IV
+
+    // 注意：如果匿名函数加了 {} ,请加 return 语句
+
+    // 加了 {return xxx}
+    new ntr().calculation(() => {return 2 * 6 / 3})  // 结果为:IV
+```
+
+---
+
+##### 注
+
+###### 罗马数字里的0
+
+罗马数字里没有 0，所以在此使用 `#` 来代替，避免计算时得出的结果包含 0
+
+> 后续可以进行splice，自行去转换
+
+###### 罗马数字的位数
+
+因为罗马数字里最大只有千位符 (M) ,所以在此进行了千位分隔符，返回的数据将呈现为 xxx,xxx,xxx
---
+++ index.js
@@ -0,0 +1,53 @@
+export default class NumberTransformRoman {
+    constructor() {
+        this.state = {
+            roman: ""
+        }
+
+        this.calculation = (callback) => {
+            if (callback) {
+                let value = callback()
+                return this.transformRoman(value)
+            }
+        }
+
+        this.transformRoman = (value) => {
+            let nSplit = this.valueSplit(value)
+            nSplit.forEach((n, idx) => {
+                this.transformN(n)
+                if (idx != nSplit.length - 1) this.state.roman += ','
+            })
+            return this.state.roman
+        }
+
+        this.valueSplit = (v) => {
+            let str = v.toString().replace(/\d{1,3}(?=(\d{3})+$)/g, "$&,")
+            return str.split(',')
+        }
+
+        this.transformN = (n) => {
+            n = n.length == 3 ? n : n.padStart(3, null)
+            for (let i = 0; i < n.length; i++) {
+                switch (i) {
+                    case 0: this.setRoman(n[i], 'C', 'D', 'M'); break;
+                    case 1: this.setRoman(n[i], 'X', 'L', 'C'); break;
+                    case 2: this.setRoman(n[i], 'I', 'V', 'X'); break;
+                }
+            }
+        }
+
+        this.setRoman = (toRoman, a, b, c) => {
+            this.state.roman += this.check(toRoman, a, b, c)
+        }
+
+        this.check = (toRoman, a, b, c) => {
+            let frequency = parseInt(toRoman)
+            if (frequency == 9) return `${a + c}`
+            else if (frequency == 0) return `#`
+            else if (frequency == 4) return `${a + b}`
+            else if (frequency == 5) return `${b}`
+            else if (frequency > 5) return `${b + a.repeat(frequency - 5)}`
+            else return a.repeat(frequency)
+        }
+    }
+}
